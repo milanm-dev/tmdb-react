e366ec16d41af0f89d2da3a60ea84938a2f3e0b0
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-08-22 11:27:58 +0200

    404 page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,17 +2,54 @@ import React from "react";
 import "./NotFound.css";
 import doggo from "./doggo.png";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 const NotFound = () => {
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
 
+  const notFoundVariant = {
+    hidden: {
+      transition: {
+        when: "afterChildren",
+      },
+    },
+    visible: {
+      transition: {
+        delay: 0.1,
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const notVariant = {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+        type: "spring",
+        stiffness: 500,
+      },
+    },
+  };
+
   return (
-    <div className="NotFound">
-      <div>404 | Page not found</div>
-      <img src={doggo} alt="doggo" />
-    </div>
+    <motion.div
+      className="NotFound"
+      variants={notFoundVariant}
+      animate="visible"
+      initial="hidden"
+    >
+      <motion.div variants={notVariant}>404 | Page not found</motion.div>
+      <motion.img variants={notVariant} src={doggo} alt="doggo" />
+    </motion.div>
   );
 };
 
